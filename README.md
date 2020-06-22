e5d12c6d23e5701932a3edc40f03d16710c77fb6
f9miao <18312700655@163.com>
2020-06-22 16:31:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+## Selenium按经纬度截取百度地图定位画面
+
++ 浏览器环境: chrome(内核83.x)
+
++ 在main.py中编辑经纬度  
+![pic](samples/sample1.png)
+
++ 运行main.py,得到截图, 截图存放在result文件夹(可能要自行创建)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import os
 import time
-import datetime
 from selenium.webdriver import Chrome
+
 
 # 批处理数据(经纬度)
 array = [
@@ -9,7 +10,7 @@ array = [
 ]
 
 chrome = Chrome(executable_path='chromedriver.exe')  # 注意chrome驱动版本要匹配
-chrome.get('file:///E:/PycharmProjects/baidu_map_screenshot/page.html')  # 静态网页加载用绝对路径
+chrome.get('file:///%s/page.html' % os.getcwd())  # 静态网页加载用绝对路径
 
 longitude = chrome.find_element_by_css_selector('#longitude')  # 精度
 latitude = chrome.find_element_by_css_selector('#latitude')  # 纬度
@@ -17,14 +18,18 @@ map = chrome.find_element_by_css_selector('#allmap')  # 地图按钮
 query = chrome.find_element_by_css_selector('#r-result > input:nth-child(3)')  # 查询按钮
 
 for item in array:
-
+    # 清空输入
     longitude.clear()
     latitude.clear()
-
+    # 输入经纬度
     longitude.send_keys(item[0])
     latitude.send_keys(item[1])
-
-    query.click()  # 点击查询按钮
+    # 点击查询按钮
+    query.click()
+    # 等待三秒后进行截图
     time.sleep(3)
+    # 截图
+    map.screenshot(filename=time.strftime("result/%Y%m%d%H%M%S{}", time.localtime()).format('.png'))
 
-    map.screenshot(filename=time.strftime("%Y%m%d%H%M%S{}", time.localtime()).format('.png'))
+chrome.close()
+print('exit!!!')
